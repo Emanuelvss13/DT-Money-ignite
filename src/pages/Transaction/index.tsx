85b00c12240fab42555f824859d33f96198c6d0f
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-06-06 11:06:25 -0300

    feat: transactions table

--- src/pages/Transaction/index.tsx
+++ src/pages/Transaction/index.tsx
@@ -1,11 +1,62 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { Pricehighlight, TransactionsContainer, TransactionsTable } from "./style";
 
 export function Transaction(){
   return (
     <>
       <Header/>
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <Pricehighlight variant="income">
+                  R$ 12.000,00
+                </Pricehighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Lanche</td>
+              <td>
+                <Pricehighlight variant="outcome">
+                  -R$ 59,00
+                </Pricehighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <Pricehighlight variant="outcome">
+                  R$ 12.000,00
+                </Pricehighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <Pricehighlight variant="income">
+                  R$ 12.000,00
+                </Pricehighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </>
   )
 }
