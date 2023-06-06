a25a7c41c88aeb461bb6f3398559b9edde360e23
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-06-06 10:19:58 -0300

    refactor: header background

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const HeaderContainer = styled.header`
   width: 100%;
   padding: 2.5rem 0 7.5rem;
+  background-color: ${(props) => props.theme["gray-900"]};
 `;
 
 export const HeaderContent = styled.div`
